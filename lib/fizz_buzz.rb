dc66122270c70fa6ed5baddef342754dada4de79
15765-akaha <turning2012@gmail.com>
2020-05-09 00:29:42 +0900

    rechalange

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -9,3 +9,34 @@ def fizz_buzz(n)
     n.to_s
   end
 end
+
+(1..100).each do |n|
+  puts fizz_buzz(n)
+end
+
+# puts "１以上の数字を入力してください"
+
+# input = gets.to_i
+
+# puts '結果は、、、'
+
+# puts fizz_buzz(input)
+
+
+# def fizz_buzz
+#   for num in 1..100 do
+    
+#     if num % 15 == 0
+#       puts "FizzBuzz"
+#     elsif num % 3 == 0
+#       puts "Fizz"
+#     elsif num % 5 == 0
+#       puts "Buzz"
+    
+#     else
+#       puts num
+#     end
+#   end
+# end
+
+# fizz_buzz
--- test/rgb_test.rb
+++ test/rgb_test.rb
@@ -1,16 +1,33 @@
+# require 'minitest/autorun'
+# require './lib/rgb'
+
+# class RgbTest < Minitest::Test
+#   def test_to_hex
+#     assert_equal '#000000', to_hex(0,0,0)
+#     assert_equal '#ffffff', to_hex(255, 255, 255)
+#     assert_equal '#043c78', to_hex(4, 60, 120)
+#   end
+
+#   def test_to_ints
+#     assert_equal [0,0,0], to_ints('#000000')
+#     assert_equal [255,255,255], to_ints('#ffffff')
+#     assert_equal [4, 60, 120], to_ints('#043c78')
+#   end
+# end
+
+
 require 'minitest/autorun'
 require './lib/rgb'
 
 class RgbTest < Minitest::Test
-  def test_to_hex
+  def test_to_hex　#3つの整数を受け取り、それぞれを１６進数に変換した文字列を返す
     assert_equal '#000000', to_hex(0,0,0)
-    assert_equal '#ffffff', to_hex(255, 255, 255)
+    assert_equal '#ffffff', to_hex(255,255,255)
     assert_equal '#043c78', to_hex(4, 60, 120)
   end
 
   def test_to_ints
-    assert_equal [0,0,0], to_ints('#000000')
-    assert_equal [255,255,255], to_ints('#ffffff')
-    assert_equal [4, 60, 120], to_ints('#043c78')
+    assert_equal [0, 0, 0], to_ints('#000000')
+    assert_equal [255, 255, 255], to_ints('#ffffff')
   end
 end
